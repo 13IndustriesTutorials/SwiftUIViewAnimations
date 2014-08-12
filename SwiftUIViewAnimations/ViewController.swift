33dd0ac1aa2890c2c25f1f1c347adf5b83eda340
dkw5877 <dkw5877@gmail.com>
2014-08-11 20:30:37 -0700

    add animation for moving view with rotation

--- SwiftUIViewAnimations/ViewController.swift
+++ SwiftUIViewAnimations/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    //outlets
     @IBOutlet weak var baseketTop: UIImageView!
     @IBOutlet weak var basketBottom: UIImageView!
+    @IBOutlet weak var fabricTop: UIImageView!
+    @IBOutlet weak var fabricBottom: UIImageView!
+    @IBOutlet weak var bug: UIImageView!
+    
+    //variables
+    var isBugDead = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,12 +42,97 @@ class ViewController: UIViewController {
             println("Basket doors opened!")
         })
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        //super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(1.0, delay: 1.2, options: .CurveEaseOut, animations: {
+            
+            //get the images current frame
+            var fabricTopFrame = self.fabricTop.frame
+            
+            //move frames origin by its height
+            fabricTopFrame.origin.y -= fabricTopFrame.size.height
+            
+            var fabricBottomFrame =  self.fabricBottom.frame
+            fabricBottomFrame.origin.y += fabricBottomFrame.size.height
+            
+            //set the new frames
+            self.fabricTop.frame = fabricTopFrame
+            self.fabricBottom.frame = fabricBottomFrame
+            
+            
+            }, completion: { finished in
+                println("Napkins opened!")
+        })
+        
+        moveBugLeft()
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    func moveBugLeft() {
+        if isBugDead { return }
+        
+        UIView.animateWithDuration(1.0, delay: 2.0, options: .CurveEaseInOut | .AllowUserInteraction, animations: {
+            self.bug.center = CGPoint(x: 75, y: 200)
+            
+            }, completion: { finished in self.faceBugRight() })
+    }
+    
+    func faceBugRight() {
+        if isBugDead { return }
+        
+        UIView.animateWithDuration(1.0, delay: 0.0, options:.CurveEaseInOut | .AllowUserInteraction , animations: {
+            self.bug.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+        
+            }, completion: { finished in self.moveBugRight() })
+    }
+    
+    func moveBugRight() {
+        
+        if isBugDead { return }
+        
+        UIView.animateWithDuration(1.0, delay: 2.0, options: .CurveEaseInOut | .AllowUserInteraction , animations: {
+            self.bug.center = CGPoint(x: 230, y: 250)
+            }, completion: {
+            finished in self.faceBugLeft()
+        })
+        
+    }
+    
+    func faceBugLeft(){
+         if isBugDead { return }
+        
+        UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveEaseInOut | .AllowUserInteraction, animations: {
+            self.bug.transform = CGAffineTransformMakeRotation(0.0)
+            }, completion: {
+                finished in  self.moveBugLeft()
+        })
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
